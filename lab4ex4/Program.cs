d812970ee720bc9b816bad9a9a0bb48a2654fbcb
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-17 20:38:15 +0300

    Add project files.

---
+++ lab4ex4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*Scrieti o functie recursiva care va afisa in ordine elementele unui vector de intregi .*/
+
+
+
+            Console.WriteLine("Introduceti lungimea vectorului: ");
+            int lungimeVector = int.Parse(Console.ReadLine());
+
+            int[] vector = new int[lungimeVector];
+
+            Console.WriteLine("Introduceti elementele vectorului: ");
+            for (int i = 0; i < lungimeVector; i++)
+            {
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            AfisareVector(vector, lungimeVector - 1);
+
+        }
+
+
+
+        static void AfisareVector(int[] v, int i)
+        {
+            if (i < -1)
+            {
+                return;
+            }
+            AfisareVector(v, i-1);
+            Console.WriteLine(v[i]);
+        }
+    }
+}        
+
+
